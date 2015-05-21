bc4e39cd9894a3d1d5ed65a9d504aa5e452d5336
Maxime RAUCH <maxime@rauch.fr>
2015-05-21 15:58:09 +0200

    Update gulpfile dist.

--- client/web/gulpfile.js
+++ client/web/gulpfile.js
@@ -1,16 +1,15 @@
 'use strict';
 
 var gulp = require('gulp');
+
 gulp.paths = {
   src: 'src',
-  dist: 'dist',
+  dist: '../../server/public',
   tmp: '.tmp',
   e2e: 'e2e'
 };
 
 require('require-dir')('./gulp');
-
-
 
 gulp.task('default', ['clean'], function () {
     gulp.start('build');
