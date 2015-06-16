a29c3cf6adda00c3359eb4b7d81fe53d7c16703f
Maxime RAUCH <maxime@rauch.fr>
2015-06-16 12:05:14 +0200

    Fix PROD constants.

--- client/web/src/app/constants/http.constant.js
+++ client/web/src/app/constants/http.constant.js
@@ -4,7 +4,7 @@ angular.module('archCas').constant('httpConstant',
   /* PROD */
   apiUrl: 'http://acrobatt-vm12.psi.ad.unistra.fr:3020',
   clientName: 'ARCH-CAS',
-  clientRedirectUri : 'http://acrobatt-vm12.psi.ad.unistra.fr:3020' */
+  clientRedirectUri : 'http://acrobatt-vm12.psi.ad.unistra.fr:3020'
 
   /* DEV
   apiUrl: 'http://localhost:3020',
